e57558e0f5d6149505649021d9569cb07c35427a
Joe Boria <joeboria@gmail.com>
2018-04-05 10:48:10 -0500

    Initial Commit

---
+++ src/main/java/com/example/groceryprices/GroceryPricesApplication.java
@@ -0,0 +1,12 @@
+package com.example.groceryprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryPricesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/groceryprices/GroceryPricesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.groceryprices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroceryPricesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
